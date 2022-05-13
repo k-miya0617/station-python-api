55d51a1a310b662dabf4fc98eb1cd0aa6b5158ad
K.Miya <51694607+k-miya0617@users.noreply.github.com>
2022-05-13 23:54:49 +0900

    例外処理を追加

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 # (pip install Flask)
 
 from flask import Flask, jsonify, request, make_response
-from pydub import AudioSegment
+from pydub import AudioSegment, exceptions
 from io import BytesIO
 
 app = Flask(__name__)
@@ -11,20 +11,28 @@ app = Flask(__name__)
 def convertAlacToFlac():
     # ファイルが含まれていない場合、エラーとして返す
     if 'file' not in request.files:
-        return jsonify({"message": "No file part in the request"})
+        return jsonify({"message": "No file part in the request"}), 400
+
+    # ファイルを取得する
     alacFile = request.files['file']
 
-    # AudioSegmentを用いてファイルを開く
-    segment = AudioSegment.from_file(BytesIO(alacFile.stream.read()), "m4a")
+    try:
+        # AudioSegmentを用いてファイルを開く
+        segment = AudioSegment.from_file(BytesIO(alacFile.stream.read()), "m4a")
 
-    # メモリーにファイルをエクスポートする
-    flacFile = segment.export(format="flac")
+        # メモリーにファイルをエクスポートする
+        flacFile = segment.export(format="flac")
 
-    # レスポンスを生成する
-    response = make_response()
-    response.data = flacFile.read()
-    response.headers['Content-Type'] = 'application/octet-stream'
-    response.headers['Content-Disposition'] = 'attachment; filename=output.flac'
-    return response
+        # レスポンスを生成する
+        response = make_response()
+        response.data = flacFile.read()
+        response.headers['Content-Type'] = 'application/octet-stream'
+        response.headers['Content-Disposition'] = 'attachment; filename=output.flac'
+
+        return response
+
+    # m4a形式以外のファイルがpostされ、正常にコンバートできなかった場合
+    except exceptions.CouldntDecodeError:
+        return jsonify({"message": "Can't open this file. Should post file in m4a (Apple lossless) format."}), 400
 
 app.run(port=8000)
