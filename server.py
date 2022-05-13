a74344bbd15546d17d693a2fdaa56c2e59ad1686
K.Miya <51694607+k-miya0617@users.noreply.github.com>
2022-05-13 23:38:02 +0900

    m4aを受け取りflacに変換し返すAPI機能の実装完了

--- server.py
+++ server.py
@@ -1,31 +1,30 @@
-# Dependencies: Flask
+# Dependencies: Flask, pydub, ffmpeg-python
 # (pip install Flask)
 
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, make_response
+from pydub import AudioSegment
+from io import BytesIO
 
 app = Flask(__name__)
 
-stores = [
-    {
-        'name': 'my_store',
-        'items': [
-            {
-                'name': 'chocolate',
-                'price': 120
-            }
-        ]
-    }
-]
+@app.route('/convert/alac-to-flac/', methods=['POST'])
+def convertAlacToFlac():
+    # ファイルが含まれていない場合、エラーとして返す
+    if 'file' not in request.files:
+        return jsonify({"message": "No file part in the request"})
+    alacFile = request.files['file']
 
-@app.route('/stores/<string:name>')
-def get_store(name):
+    # AudioSegmentを用いてファイルを開く
+    segment = AudioSegment.from_file(BytesIO(alacFile.stream.read()), "m4a")
 
-    print("INPUT " + name)
+    # メモリーにファイルをエクスポートする
+    flacFile = segment.export(format="flac")
 
-    for store in stores:
-        if store["name"] == name:
-            return jsonify(store)
-
-    return jsonify({"message": "no store named {} found".format(name)})
+    # レスポンスを生成する
+    response = make_response()
+    response.data = flacFile.read()
+    response.headers['Content-Type'] = 'application/octet-stream'
+    response.headers['Content-Disposition'] = 'attachment; filename=output.flac'
+    return response
 
 app.run(port=8000)
